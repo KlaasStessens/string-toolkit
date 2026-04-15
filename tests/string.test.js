0288c130d8e52f088d29ee57338bc02c36e0a6bf
Klaas Stessens <klaas.stessens@student.hogent.be>
2026-04-15 14:16:40 +0200

    feat: kebab test

--- tests/string.test.js
+++ tests/string.test.js
@@ -3,3 +3,9 @@ const { toSnakeCase } = require("../index");
 test("converts string to snake_case", () => {
   expect(toSnakeCase("Hello World")).toBe("hello_world");
 });
+
+const { toKebabCase } = require("../index");
+
+test("converts string to kebab-case", () => {
+  expect(toKebabCase("Hello World")).toBe("hello-world");
+});
